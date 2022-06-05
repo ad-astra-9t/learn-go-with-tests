0edd1cfe2233806b3aa5804a3af356200cb1d4a1
Dean Lin <richer6605@gmail.com>
2022-06-05 18:04:33 +0800

    feat: Add `Sum` function

--- array-slice/main.go
+++ array-slice/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func Sum(nums []int) int {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	return sum
+}
+
 func main() {
 	fmt.Println("Hello world!")
 }
---
+++ array-slice/sum_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	nums := []int{1, 2, 3, 4, 5}
+	got := Sum(nums)
+	want := 15
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
